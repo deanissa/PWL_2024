8ba50d3fac2b98510cdd510d5e47d2202bd7f529
deanissa <deanissa604@gmail.com>
2025-02-19 14:04:10 +0700

    Basic Routing

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2341760187';
 });
